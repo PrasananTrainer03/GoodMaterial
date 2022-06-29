e2e7b9c3817f3fcf53a72b478e4aedb0583ee239
PrasananTrainer03 <prassup.dotnet@gmail.com>
2022-06-29 08:50:53 +0530

    code

---
+++ June_29_Exam/Hexa1_Exam/LeaveDetails.java
@@ -0,0 +1,52 @@
+package com.java.abs;
+
+import java.util.Date;
+
+public class LeaveDetails {
+
+	private int leaveId;
+	private int empId;
+	private Date leaveStartDate;
+	private Date leaveEndDate;
+	private int noOfDays;
+	private String leaveReason;
+	
+	public int getLeaveId() {
+		return leaveId;
+	}
+	public void setLeaveId(int leaveId) {
+		this.leaveId = leaveId;
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public Date getLeaveStartDate() {
+		return leaveStartDate;
+	}
+	public void setLeaveStartDate(Date leaveStartDate) {
+		this.leaveStartDate = leaveStartDate;
+	}
+	public Date getLeaveEndDate() {
+		return leaveEndDate;
+	}
+	public void setLeaveEndDate(Date leaveEndDate) {
+		this.leaveEndDate = leaveEndDate;
+	}
+	public int getNoOfDays() {
+		return noOfDays;
+	}
+	public void setNoOfDays(int noOfDays) {
+		this.noOfDays = noOfDays;
+	}
+	public String getLeaveReason() {
+		return leaveReason;
+	}
+	public void setLeaveReason(String leaveReason) {
+		this.leaveReason = leaveReason;
+	}
+	
+	
+}
