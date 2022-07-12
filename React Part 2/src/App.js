a081298185ecf8362f4cf92a454c0ac1ef6cba24
PrasananTrainer03 <prassup.dotnet@gmail.com>
2022-07-12 16:28:33 +0530

    code

---
+++ React Part 2/src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+import First from './components/first/first';
+import Second from './components/second/second';
+import Third from './components/third/third';
+import Fourth from './components/fourth/fourth';
+import Fifth from './components/fifth/fifth';
+import Six from './components/six/six';
+import Seven from './components/seven/seven';
+import NameForm from './components/nameform/nameform';
+import MultiForm from './components/multiform/multiform';
+import ButtonExample from './components/buttonexample/buttonexample';
+import FormExampleNew from './components/formExampleNew/formExampleNew';
+import StudentForm from './components/studentForm/studentForm';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Welcome to ReactJs...Prasanna Trainer Thank You
+        </p>
+        <First></First>
+        <Second></Second>
+        <Third></Third>
+        <Fourth></Fourth>
+        <Fifth></Fifth>
+        <ButtonExample></ButtonExample>
+        <FormExampleNew></FormExampleNew>
+        <StudentForm></StudentForm>
+        <Six user="Prasanna"></Six>
+        <Seven firstName="Prasanna" lastName="Pappu"></Seven>
+        <NameForm></NameForm>
+        <MultiForm></MultiForm>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ React Part 2/src/components/seven/seven.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './seven.scss'
+// import { connect } from "react-redux";
+// import { bindActionCreators } from "redux";
+// import * as sevenActions from "../../store/seven/actions";
+export default class seven extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {};
+    // }
+    render() {
+      return <div className="component-seven">
+        Hi My FirstName is {this.props.firstName} Last Name is {this.props.lastName}</div>;
+    }
+  }
+// export default connect(
+//     ({ seven }) => ({ ...seven }),
+//     dispatch => bindActionCreators({ ...sevenActions }, dispatch)
+//   )( seven );
